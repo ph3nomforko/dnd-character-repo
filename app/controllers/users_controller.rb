96fc256c494ca5f73bac8a7211c4d573da86d8a0
ph3nomforko <codi.kraus@gmail.com>
2021-08-18 12:59:04 -0500

    create basic form for 'login.erb'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
         erb :login
     end
 
+    post '/login' do # Receive login form, find the user, and create a session
+        
+    end
+
     get '/signup' do
 
     end
