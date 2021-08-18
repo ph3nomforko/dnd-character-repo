ac6c4e33f4fba85ca6d1fad071f2b351f84826d4
ph3nomforko <codi.kraus@gmail.com>
2021-08-18 14:01:25 -0500

    finish route of authen. user to test show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
     post '/login' do # Receive login form, find the user, and create a session
         @user = User.find_by(email: params[:email])
         if @user.authenticate(params[:password])
-            
+            session[:user_id] = @user.id
+            redirect "/users/#{@user.id}"
         else
             # Give error message if wrong credentials
         end
@@ -15,6 +16,10 @@ class UsersController < ApplicationController
 
     get '/signup' do
 
+    end
+
+    get '/users/:id' do
+        "User show page"
     end
 
 end
