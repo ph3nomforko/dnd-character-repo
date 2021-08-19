3c3a86fe6af8a14a321e62fc158ae769559856b1
ph3nomforko <codi.kraus@gmail.com>
2021-08-19 11:45:53 -0500

    create basic logic to prevent non-assoc chars

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -25,19 +25,38 @@ class CharactersController < ApplicationController
 
     get '/characters/:id/edit' do
         find_and_set_character
-        erb :'/characters/edit'
+        if logged_in?
+            if @character.user == current_user
+                erb :'/characters/edit'
+            else
+                redirect "/users/#{current_user.id}"
+            end
+        else
+            redirect '/'
+        end
     end
 
     patch '/characters/:id' do
         find_and_set_character
-        @character.update(character_name: params[:character_name], character_class: params[:character_class],
-            species: params[:species], level: params[:level])
-        redirect "/characters/#{@character.id}"
+        if logged_in?
+            if @character.user == current_user
+                @character.update(character_name: params[:character_name], character_class: params[:character_class],
+                    species: params[:species], level: params[:level])
+            else
+                redirect "/users/#{current_user.id}"
+            end
+        else
+            redirect '/'
+        end
     end
+
+#    get '/characters/index' do
+#        erb :'/characters/index'
+#    end
 
     private
     def find_and_set_character
-        @character = Character.find_by(id: params[:id])
+        @character = Character.find(params[:id])
     end
 
 end
