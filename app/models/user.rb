698ebd5bfd87c7848987f6b7aa083e6dd65c052c
ph3nomforko <codi.kraus@gmail.com>
2021-08-19 17:00:16 -0500

    include validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
     has_secure_password
 
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
+
     has_many :characters
 
 end
