3ac7c76d0f3c12d3b031ad2ea8d86c87badfdc95
ph3nomforko <codi.kraus@gmail.com>
2021-08-19 09:10:40 -0500

    add content check and create character

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -5,7 +5,17 @@ class CharactersController < ApplicationController
     end
 
     post '/characters' do
+        if !logged_in?
+            redirect '/'
+        end
 
+        if params[:name] != "" && params[:character_class] != "" && params[:species] != "" && params[:level] != ""
+            @character = Character.create(name: params[:name], character_class: params[:character_class],
+                species: params[:species], level: params[:level], user_id: current_user.id)
+            redirect "/characters/#{@character.id}"
+        else
+            redirect '/characters/new'
+        end
     end
 
 end
