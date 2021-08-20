48f6f8a18c35b5ee41c40955937aabf8cf1e5a96
ph3nomforko <codi.kraus@gmail.com>
2021-08-20 09:44:28 -0500

    remove binding.pry

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -43,7 +43,6 @@ class CharactersController < ApplicationController
         find_and_set_character
         redirect_if_not_logged_in
         if authorized_to_edit?(@character) && @character.valid?
-            binding.pry
             flash[:message] = "You've successfully updated your character!"
             @character.update(character_name: params[:character_name], character_class: params[:character_class],
                 species: params[:species], level: params[:level])
