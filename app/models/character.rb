6f0eff4510348a639138a7a609fc00344dd5a1ea
ph3nomforko <codi.kraus@gmail.com>
2021-08-19 19:36:42 -0500

    add AR validations to character model

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,5 +1,10 @@
 class Character < ActiveRecord::Base
 
+    validates :character_name, presence: true
+    validates :character_class, presence: true
+    validates :species, presence: true
+    validates :level, presence: true
+
     belongs_to :user
     
 end
